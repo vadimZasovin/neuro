17b6fd16b226430e7402a3aa5711ed018e9696d3
vadimZasovin <vzasovin@gmail.com>
2017-12-04 14:26:52 +0300

    fixed AggregationFunction sum

--- src/main/kotlin/com/imogene/neuro/AggregationFunctions.kt
+++ src/main/kotlin/com/imogene/neuro/AggregationFunctions.kt
@@ -5,8 +5,8 @@ object AggregationFunctions {
     fun sum() = object : AggregationFunction {
         override fun aggregate(inputs: DoubleArray, memory: NeuronMemory, bias: Double): Double {
             var result = 0.0
-            for ((index, input) in inputs.withIndex()){
-                val weight = memory[index]
+            inputs.forEachIndexed { i, input ->
+                val weight = memory[i]
                 result += input * weight
             }
             result += bias
